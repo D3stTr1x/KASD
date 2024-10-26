6e74ee964d374941ad3b28b710a02cde74e9d65b
D3stTr1x <dima.tertishniy@yandex.ru>
2024-10-26 11:57:07 +0300

    Add files via upload

---
+++ Lab11/Lab11/Array.cs
@@ -0,0 +1,274 @@
+﻿namespace MyArray
+{
+    public class MyArrayList<T>
+    {
+        private int size;
+        T[] elementData;
+        public MyArrayList() // Создание пустого динамического массива
+        {
+            elementData = Array.Empty<T>();
+            size = 0;
+        }
+
+        public MyArrayList(T[] a) //  создания динамического массива и заполнения его элементами из передаваемого массива a.
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+            elementData = new T[(int)(a.Length)];
+            for (int i = 0; i < a.Length; i++)
+                elementData[i] = a[i];
+            size = a.Length;
+        }
+
+        public MyArrayList(int capacity) //создания пустого динамического массива с внутренним массивом, размер которого будет равен значению параметра capacity.
+
+        {
+            elementData = new T[capacity];
+            size = capacity;
+        }
+
+        public void Add(T e) // для добавления элемента в конец динамического массива.
+        {
+            // Проверяем, есть ли место в массиве, если нет — увеличиваем его
+            if (size == elementData.Length)
+            {
+                T[] newData = new T[elementData.Length * 2];
+                for (int i = 0; i < elementData.Length; i++)
+                {
+                    newData[i] = elementData[i];
+                }
+                elementData = newData;
+            }
+
+            // Добавляем новый элемент и увеличиваем размер
+            elementData[size] = e;
+            size++;
+        }
+
+        public void AddAll(T[] a) // для добавления элементов из массива
+        {
+            foreach (T item in a)
+                Add(item);
+        }
+
+        public void Clear() //  для удаления всех элементов из динамического массива
+        {
+            elementData = Array.Empty<T>();
+            size = 0;
+        }
+
+        public bool Contains(object o) //  для проверки, находится ли указанный объект в динамическом массиве.
+        {
+            if (elementData != null && size != 0)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (o.Equals(elementData[i]))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsAll(T[] a) // для проверки, содержатся ли указанные объекты в динамическом массиве.
+        {
+            bool f = false;
+            foreach (T item in a)
+                if (item != null)
+                    f = Contains(item);
+            return f;
+        }
+
+        public bool IsEmpty() //для проверки, является ли динамический массив пустым.
+        {
+            if (size == 0)
+                return true;
+            else return false;
+        }
+
+        public void Remove(object o) // для удаления указанного объекта из динамического массива, если он есть там.
+        {
+            if (o == null || elementData == null)
+            {
+                return;
+            }
+
+            if (Contains(o))
+            {
+                int index = IndexOf(o);
+
+                if (index >= 0)
+                {
+                    for (int i = index; i < size - 1; i++)
+                    {
+                        elementData[i] = elementData[i + 1];
+                    }
+                    elementData[size - 1] = default!; // Для значимых типов присваиваем значение по умолчанию
+                    size--;
+                }
+            }
+        }
+
+        public void RemoveAll(T[] a) //  для удаления указанных объектов из динамического массива.
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+            foreach (T item in a)
+                if (item != null)
+                    Remove(item);
+        }
+
+        public void RetainAll(T[] a) // для оставления в динамическом массиве только указанных объектов
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+            T[] newArray = new T[size];
+            int newSize = 0;
+            foreach (T item in a)
+                for (int i = 0; i < size; i++)
+                    if (item != null)
+                        if (item.Equals(elementData[i]))
+                        {
+                            newArray[newSize] = elementData[i];
+                            newSize++;
+                        }
+            elementData = newArray;
+            size = newSize;
+        }
+
+        public int Size() //  для получения размера динамического массива в элементах.
+        {
+            return size;
+        }
+
+        public T[] ToArray() // для возвращения массива объектов, содержащего все элементы динамического массива
+        {
+            T[] a = new T[size]; // Создаём массив типа T[] с размером size
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = elementData[i]; // Копируем элементы
+            }
+            return a;
+        }
+
+        public T[] ToArray(T[] a) //для возвращения массива объектов, содержащего все элементы динамического массива.Если аргумент a равен null, то создаётся новыймассив, в который копируются элементы
+        {
+            if (a.Length < size)
+            {
+                a = new T[size];
+            }
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = elementData[i];
+            }
+            return a;
+
+        }
+        
+
+        public void Add(int index, T e) //для добавления элемента в указанную позицию
+        {
+            if (index < 0 || index >= size)
+                throw new ArgumentOutOfRangeException("index");
+
+            if (elementData == null)
+                elementData = new T[1];
+
+            if (size == elementData.Length)
+            {
+                T[] newArray = new T[size + size / 2 + 1];
+                for (int i = 0; i < size; i++)
+                    newArray[i] = elementData[i];
+                elementData = newArray;
+            }
+
+            for (int i = size; i > index; i--)
+                elementData[i] = elementData[i - 1];
+
+            elementData[index] = e;
+            size++;
+        }
+
+        public void AddAll(int index, T[] a) //для добавления элементов в указанную позицию
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+            foreach (T item in a)
+            {
+                Add(index, item);
+                index++;
+            }
+        }
+
+        public T Get(int index) //для возвращения элемента в указанной позиции.
+        {
+            if (index < 0 || index >= size)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            return elementData[index];
+        }
+
+        public int IndexOf(object o) //для возвращения индекса указанного объекта, или -1, если его нет в динамическом массиве
+        {
+            for (int i = 0; i < size; i++)
+                if (o.Equals(elementData[i]))
+                    return i;
+            return -1;
+        }
+
+        public int LastIndexOf(object o) //для нахождения последнего вхождения указанного объекта, или -1, если его нет в динамическом массиве
+        {
+            for (int i = size - 1; i >= 0; i--)
+                if (o.Equals(elementData[i]))
+                    return i;
+            return -1;
+
+        }
+
+        public T RemoveInd(int index) //для удаления и возвращения элемента в указанной позиции
+        {
+            if (index < 0 || index >= size)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            T e = elementData[index];
+            if (e != null)
+                Remove(e);
+            return e;
+        }
+
+        public void Set(int index, T e)//для замены элемента в указанной позиции новым элементом
+        {
+            if (index < 0 || index >= size)
+                throw new ArgumentOutOfRangeException(nameof(index));
+            if (e == null)
+                throw new ArgumentNullException();
+            elementData[index] = e;
+        }
+
+        public MyArrayList<T> SubList(int fromIndex, int toIndex) //для возвращения части динамического массива, т.е. элементов в диапазоне [fromIndex; toIndex).
+        {
+            if (fromIndex > toIndex)
+                throw new ArgumentException("fromIndex > toIndex");
+            if (fromIndex < 0 || fromIndex >= size)
+                throw new ArgumentOutOfRangeException("fromIndex");
+            if (toIndex < 0 || toIndex >= size)
+                throw new ArgumentOutOfRangeException("toIndex");
+            MyArrayList<T> list = new MyArrayList<T>(toIndex - fromIndex);
+            for (int i = 0; i < list.size; i++)
+                list.Set(i, elementData[i + fromIndex]);
+            return list;
+
+        }
+
+        public void PrintArray()
+        {
+            if (size != 0)
+            {
+                for (int i = 0; i < size; i++)
+                    Console.Write($"{elementData[i]} ");
+                Console.WriteLine();
+            }
+            else Console.WriteLine("Empty array");
+        }
+    }
+}
---
+++ Lab11/Lab11/Heap.cs
@@ -0,0 +1,98 @@
+﻿using MyArray;
+
+namespace MyHeap
+{
+    class MyHeap<T> where T : IComparable<T>
+    {
+        public MyArrayList<T> heap = new MyArrayList<T>();
+        private int size = 0;
+        public MyHeap(T[] array)// Конструктор для создания кучи из массива
+        {
+            size = array.Length;
+            for (int i = 0; i < size; i++)
+                heap.Add(array[i]);
+            for (int i = size / 2 - 1; i >= 0; i--)
+                RestoringHeap(i);
+        }
+        public T SearchMax()// Метод для нахождения максимума кучи
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Heap is empty.");
+            return heap.Get(0);
+        }
+        public T RemoveMax() // Метод для удаления максимума кучи
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Heap is empty.");
+            T maximum = heap.Get(0);
+            heap.Set(0, heap.Get(size - 1));
+            size--;
+            RestoringHeap(0);
+            return maximum;
+        }
+        public void KeyIncr(int index, T e)// Метод для увеличения ключа
+        {
+            if (index >= size)
+                throw new IndexOutOfRangeException("Index out of range.");
+            if (heap.Get(index).CompareTo(e) > 0)
+                throw new InvalidOperationException("New key is smaller than the current key.");
+            heap.Set(index, e);
+            RestoringHeapUp(index);
+        }
+        public void AddElement(T element)// Метод для добавления нового элемента
+        {
+            heap.Add(element);
+            size++;
+            RestoringHeapUp(size - 1);
+        }
+        public void HeapMerge(MyHeap<T> newHeap)// Метод для слияния куч
+        {
+            while (newHeap.size > 0)
+            {
+                T element = newHeap.RemoveMax();
+                AddElement(element);
+            }
+        }
+
+        private void RestoringHeap(int i)// Восстановление свойства кучи
+        {
+            int left;
+            int right;
+            int parents = i;
+            while (true)
+            {
+                left = 2 * i + 1;
+                right = 2 * i + 2;
+                if (right < size && heap.Get(right).CompareTo(heap.Get(parents)) > 0)
+                    parents = right;
+
+                if (left < size && heap.Get(left).CompareTo(heap.Get(parents)) > 0)
+                    parents = left;
+                if (parents == i) break;
+                T temp = heap.Get(parents);
+                heap.Set(parents, heap.Get(i));
+                heap.Set(i, temp);
+
+                i = parents;
+            }
+        }
+        private void RestoringHeapUp(int i)// Восстановление свойства кучи вверх
+        {
+            while (i > 0)
+            {
+                int parent = (i - 1) / 2;
+                if (heap.Get(i).CompareTo(heap.Get(parent)) <= 0) break;
+                T temp = heap.Get(i);
+                heap.Set(i, heap.Get(parent));
+                heap.Set(parent, temp);
+                i = parent;
+            }
+        }
+        public void Print()// Метод для вывода кучи
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(heap.Get(i) + " ");
+            Console.WriteLine();
+        }
+    }
+}
