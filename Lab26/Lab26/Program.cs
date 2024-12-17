d71545de02a100d2b1335106ed8a572ced76098d
D3stTr1x <dima.tertishniy@yandex.ru>
2024-12-17 14:14:10 +0300

    Add files via upload

---
+++ Lab26/Lab26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using MyHashSet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Regex wordRegex = new Regex(@"\b[a-zA-Z]+\b");
+        MyHashSet<string> uniqueWords = new MyHashSet<string>();
+        try
+        {
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                var matches = wordRegex.Matches(line);
+                foreach (Match match in matches)
+                {                    
+                    uniqueWords.Add(match.Value.ToLower());
+                }
+            }            
+            Console.WriteLine("Уникальные слова в файле:");
+            foreach (var word in uniqueWords.ToArray())
+            {
+                Console.WriteLine(word);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
